0e18cebc0576ed059b508812ef9f1d654b97d106
Jacob Haas <jacob_haas_7@hotmail.com>
2019-08-05 19:01:03 -0700

    image alignment, start of animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import granite from './assests/images/granite.jpg';
+import granite from './assests/images/granite1.jpg';
 import gneiss from './assests/images/gneiss1.jpg';
-import sedimentary from './assests/images/sedimentary.jpg';
+import sedimentary from './assests/images/sedimentary1.jpg';
 
 function App() {
   return (
@@ -12,25 +12,34 @@ function App() {
       </header>
       <body>
         <div className="bodyWrapper">
-          {/* <ul> */}
+          <ul className="circle-container">
 
-          <div className="rockTypeDiv">
-            {/* <li> */}
+            <li>
+          <div className="rockTypeDiv1">
+            <div className="hiddenDiv">
             <img src={granite} alt="Igneous" className="rockTypeImg"></img>
-            {/* </li> */}
+            <p>hasdfasdfa</p>
+            </div>
           </div>
-          <div className="rockTypeDiv">
-            {/* <li> */}
+            </li>
+            <li>
+          <div className="rockTypeDiv2">
+            <div className="hiddenDiv">
             <img src={gneiss} alt="Metamorphic" className="rockTypeImg"></img>
-            {/* </li> */}
+            <p className="rockText">hasdfasdfa</p>
           </div>
-          <div className="rockTypeDiv">
-            {/* <li> */}
+          </div>
+            </li>
+            <li>
+            <div className="rockTypeDiv3">
+          <div className="hiddenDiv">
             <img src={sedimentary} alt="Sedimentary" className="rockTypeImg"></img>
-            {/* </li> */}
+            <p>hasdfasdfa</p>
           </div>
+          </div>
+            </li>
 
-          {/* </ul> */}
+          </ul>
         </div>
         <div>
 
